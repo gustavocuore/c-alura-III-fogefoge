a17dc998e10bfa878cf83f1022f16534ebf12bb1
gustavocuore <gucuore@gmail.com>
2022-03-28 16:01:17 -0300

    @gustavocuore [TASK: Projeto Foge Foge]

---
+++ ui.h
@@ -0,0 +1,10 @@
+#ifndef _UI_H_ //é um if em tempo de compilação (para o compilador)
+#define _UI_H_
+
+#include "mapa.h"
+
+void imprimemapa(MAPA* m);
+
+void imprimeparte(char desenho[4][7], int parte);
+
+#endif
